d8a0af4463cf15ac10d4c44a2c1b64c16c7205dd
erblack <erblack@practicum.com>
2023-06-12 15:35:37 +0200

    sync today

--- bin/syncToday.js
+++ bin/syncToday.js
@@ -4,6 +4,7 @@ const TodoistApi = require('../lib/api');
 const fs = require('fs');
 const syncFormat = require('../lib/syncFormat/syncFormat');
 const replaceContent = require('../lib/syncFormat/replaceContent');
+const TodoistSyncApi = require('../lib/sync-api');
 
 try {
     require('dotenv').config();
@@ -11,6 +12,7 @@ try {
 
 const program = new commander.Command();
 const api = new TodoistApi(process.env.TODOIST_TOKEN);
+const syncApi = new TodoistSyncApi(process.env.TODOIST_TOKEN);
 
 program
     .version(require('../package').version)
@@ -29,6 +31,16 @@ program
                 filter: 'today',
             })
         ).data;
+
+        const completed = (await syncApi.getCompletedToday()).data;
+
+        for (const { task_id } of completed.items) {
+            try {
+                const task = (await syncApi.getItem(task_id)).data.item;
+
+                tasks.push({ ...task, is_completed: true });
+            } catch (e) {}
+        }
 
         const tasksContent = tasks.map(syncFormat).join('\n');
 
